d864eb460be55c9171110f54b2b33eb5c09e809e
Luis Augusto Torres de Carvalho <guguptb@gmail.com>
2022-11-11 00:47:35 -0300

    Incluido docstrings.
    Apareceu um bug quando digitanmos valores numericos diferentes do permitido

--- ConverteTemperatura.py
+++ ConverteTemperatura.py
@@ -1,20 +1,40 @@
-import os
-from getpass import _raw_input
-from os import system
+import msvcrt as m
+from os import system  # Utilizar função cls do terminal
 
 
-def gkfv(val: object, dicionario: object) -> object:
+def WaitEnter():
+    m.getch()
+
+
+def gkfv(val: str, dicio: dict) -> object:
     """
-    Função busca val em dicionário e retorna a primeira chave correspondente encontrada, ou None
+    gkfv - Get Key From Key - Função busca val em dicionário e retorna a
+    primeira chave correspondente encontrada, ou None
 
+    Args:
+        val (str): Valor a ser buscado no objeto tipo dicionario
+        dicio (dict): Objeto tipo dicionário onde o valor será procurado
+
+    Returns:
+        object: Retorna a chave referente ao primeiro valor encontrado, se não retorna None
     """
     lista = [chave for chave, valor in dicio.items() if valor == val]
     if lista:
         return lista[0]
     return None
 
-dic
-def Menu(opcoes_menu, descricao_opcoes):
+
+def Menu(opcoes_menu: dict, descricao_opcoes: str):
+    """
+    Menu Exibe menu baseado nos argumentos de entrada e pede ao usuario que esclha uma opção do menu.
+
+    Args:
+        opcoes_menu (dict): Objeto contendo texto do menu (chave) e letra da escolha(valor). ex {'ºC (converte ºC -> ºF)': 'C'}
+        descricao_opcoes (str): Texto de escolha a ser exibido no menu.
+
+    Returns:
+        str: Retorna valor baseado no valor de opcoes_menu (objeto dict) selecionado pelo usuario
+    """
     lista_valida = []
     index = 0
     opcoes_menu.update({'Sair': 'S'})
@@ -27,44 +47,55 @@ def Menu(opcoes_menu, descricao_opcoes):
 
     while not validacao_input:
         try:
-            inputRaw = input(f'Selecione uma opção: ')
+            inputRaw = input('Selecione uma opção: ')
             inputNo = int(inputRaw) - 1
             if int(inputRaw) == len(lista_valida):
                 return 'S'
             elif -1 < inputNo < len(lista_valida):
                 opcao_selecionada = lista_valida[inputNo]
-                print(f'Opção selecionada: {gkfv(opcao_selecionada,opcoes_menu)} ')
+                print(
+                    f'Opção selecionada: {gkfv(opcao_selecionada,opcoes_menu)} ')
                 validacao_input = True
             else:
                 print(f'Por favor, selecione uma {descricao_opcoes} válida')
-                os.system('cls')
+                system('cls')
                 Menu(opcoes_menu, descricao_opcoes)
         except ValueError:
             print('\n' + '!\n' * 3)
-            input(f'!!!!  Permitido apenas números  !!!!\n(Aperte enter para continuar)')
-            os.system('cls')
+            print('!!!!  Permitido apenas números  !!!!\n(Aperte enter para continuar)')
+            WaitEnter()
+            system('cls')
             Menu(opcoes_menu, descricao_opcoes)
 
     return opcao_selecionada
 
 
-def Conversao(opcao_conversao):
+def Conversao(opcao_conversao: str):
+    """
+    Conversao Função para conversão de temperatura baseada na escolha do usuario na função Menu.
+
+    Args:
+        opcao_conversao (str): Valor retornado pela função menu.
+    """
+
     try:
         if opcao_conversao == 'C':
             celsius = float(input("Insira a temperatura em ºC (Celsius): "))
             fahrenheit = (celsius * 9 / 5) + 32
             print(f'{celsius:.1f} ºC (Celsius) é: {fahrenheit:.1f} ºF (Fahrenheit)')
         elif opcao_conversao == 'F':
-            fahrenheit = float(input('Insira a temperatura em ºF (Fahrenheit): '))
+            fahrenheit = float(
+                input('Insira a temperatura em ºF (Fahrenheit): '))
             celsius = (fahrenheit - 32) * 5 / 9
             print(f'{fahrenheit:.1f} ºF (Fahrenheit) é: {celsius:.1f} ºC (Celsius)')
         else:
-            _raw_input('Opção invalida tente novamente')
+            print('Opção invalida tente novamente')
+            WaitEnter()
     except ValueError:
         print('\n' + '!\n' * 3)
         print('!!!!  Apenas números são permitidos  !!!!')
-        _raw_input('Aperte \'ENTER\' para continuar')
-        os.system('cls')
+        WaitEnter()
+        system('cls')
         Conversao(opcao_conversao)
 
 
